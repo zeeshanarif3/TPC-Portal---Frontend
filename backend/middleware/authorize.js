b89cd270dca7dbd9695920ed9b4d7c7a6ff703d2
Jaspreet100101 <jaspreet99100@gmail.com>
2026-08-17 14:03:46 +0530

    changed ui

--- backend/middleware/authorize.js
+++ backend/middleware/authorize.js
@@ -13,8 +13,8 @@
 
 const authorizeRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    console.log('req.user:', req.user);
-    console.log('allowedRoles:', allowedRoles);
+    // console.log('req.user:', req.user);
+    // console.log('allowedRoles:', allowedRoles);
     if (!req.user || !allowedRoles.includes(req.user.role)) {
       return res.status(403).json({ message: "Forbidden: You do not have access" });
     }
